f37fa00b18bee97169c49fd861f50b9ca1b9b73c
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2018-06-04 11:33:07 -0400

    Create defs.h

---
+++ defs.h
@@ -0,0 +1,8 @@
+struct exe_header {
+    unsigned short magic_num;
+    unsigned short numLastBlock;
+    unsigned short numBlocks;
+    unsigned short numRelocEntries;
+    unsigned short numParagraphsHead;
+    unsigned short numParagraphsAdd;
+    unsigned short 
